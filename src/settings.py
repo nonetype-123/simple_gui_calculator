26aad5bd2ca5ca03dbad869b30ac64f50b26143a
nonetype <objectobject039@gmail.com>
2025-03-24 00:49:04 +0300

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -4,7 +4,7 @@ import json as jn
 sys.path.append("../conf")
 
 with open('conf.json', 'r') as conf_file:
-    data = json.load(conf_file)
+    data = jn.load(conf_file)
     print(data)
 
 while True:

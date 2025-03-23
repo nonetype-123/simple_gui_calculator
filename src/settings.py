8b41c2befddfcc68282a272db12eedbe5b46b202
nonetype <objectobject039@gmail.com>
2025-03-24 00:48:37 +0300

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -6,3 +6,12 @@ sys.path.append("../conf")
 with open('conf.json', 'r') as conf_file:
     data = json.load(conf_file)
     print(data)
+
+while True:
+    key = input("Which key do you want to change? : ")
+    
+    # parse
+
+    break
+    
+# coming soon...
